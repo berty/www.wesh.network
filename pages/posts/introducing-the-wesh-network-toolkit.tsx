2394b0d33f32ae748af0a87b68dfebb1d1122668
Mehrad Mahmoudian <m.mahmoudian@gmail.com>
2025-06-19 21:00:03 +0300

    bump the copyright year to 2025 (#37)

--- pages/posts/introducing-the-wesh-network-toolkit.tsx
+++ pages/posts/introducing-the-wesh-network-toolkit.tsx
@@ -41,7 +41,7 @@ export default function Post() {
 
         <Footer />
         <div className={styles.credits}>
-          <p>Copyright © 2023 – Berty.Tech non-profit organization</p>
+          <p>Copyright © 2025 – Berty.Tech non-profit organization</p>
           <div>
             {/* <img src="./img/Telegram.png" alt="" /> */ }
             {/* <img src="./img/Twitter.png" alt="" /> */ }
--- pages/posts/wesh-hello-world-app.tsx
+++ pages/posts/wesh-hello-world-app.tsx
@@ -147,7 +147,7 @@ go run .`}</code>
 
         <Footer />
         <div className={styles.credits}>
-          <p>Copyright © 2023 – Berty.Tech non-profit organization</p>
+          <p>Copyright © 2025 – Berty.Tech non-profit organization</p>
           <div>
             {/* <img src="./img/Telegram.png" alt="" /> */}
             {/* <img src="./img/Twitter.png" alt="" /> */}
